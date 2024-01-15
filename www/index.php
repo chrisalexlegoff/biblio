0fd8a9952c32aff968f190e3d266b4e8a0399586
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2024-01-15 10:35:02 +0100

    Update Index.php

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- based ccs bootstrap by bootswatch -->
     <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
-    <title>Biblio</title>
+    <title>Biblio2</title>
 </head>
 
 <body>
